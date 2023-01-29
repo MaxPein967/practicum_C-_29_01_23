2d8d8accae8fa7d52d63aad5c92bd8cbe8a637c2
Sergei Vasiliev <9670105747@mail.ru>
2023-01-29 15:29:09 +0700

    Является ли первое число квадратом второго

---
+++ C#1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+// а = 25, b = 5 -> да
+// а = 2, b = 10 -> нет
+// а = 9, b = -3 -> да
+// а = -3, b = 9 -> нет
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = numberB * numberB;
+if(result == numberA)
+{
+    Console.WriteLine($"Результат: {result} Да");
+}
+else
+{
+    Console.WriteLine($"Результат: {result} Нет");
+}
