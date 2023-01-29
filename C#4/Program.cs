4ec26c5843f249d6ab7c1f5a1a407e81d96ac396
Sergei Vasiliev <9670105747@mail.ru>
2023-01-29 16:41:53 +0700

    Задача про техзначное число

---
+++ C#4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int b = 100;
+int c = 999;
+if (number >= b && number <= c)
+{
+    string strNumber = number.ToString();
+    char a = strNumber[strNumber.Length - 1];
+    Console.WriteLine("Число: " + number);
+    Console.WriteLine("Единицы: " + a);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число ");
+    Console.ReadKey();
+}
+
